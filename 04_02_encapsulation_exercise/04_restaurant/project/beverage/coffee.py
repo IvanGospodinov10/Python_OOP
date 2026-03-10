69213ab8b4de1f0513469a69ffa1fccf498f9119
Ivan Gospodinov <ivan9069@gmail.com>
2026-03-10 21:03:44 +0200

    add project restaurant

--- 04_02_encapsulation_exercise/04_restaurant/project/beverage/coffee.py
+++ 04_02_encapsulation_exercise/04_restaurant/project/beverage/coffee.py
@@ -5,7 +5,7 @@ class Coffee(HotBeverage):
     MILLILITERS = 50
     PRICE = 3.50
 
-    def init_beverage(self, name: str, caffeine: float):
+    def __init__(self, name: str, caffeine: float):
         super().__init__(name, Coffee.PRICE, Coffee.MILLILITERS)
         self.__caffeine = caffeine
 
