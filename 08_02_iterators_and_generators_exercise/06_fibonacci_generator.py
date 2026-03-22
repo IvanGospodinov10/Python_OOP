61d633b530c9b6c9749c926ad2f871ff136a210b
Ivan Gospodinov <ivan9069@gmail.com>
2026-03-22 07:48:35 +0200

    add file 08_02_iterators_and_generators_exercise/06_fibonacci_generator.py

---
+++ 08_02_iterators_and_generators_exercise/06_fibonacci_generator.py
@@ -0,0 +1,17 @@
+def fibonacci():
+    curr_num, next_num = 0, 1
+
+    while True:
+        yield curr_num
+        curr_num, next_num = next_num, curr_num + next_num
+
+
+generator = fibonacci()
+for i in range(10):
+    print(next(generator))
+
+print("=======================================")
+
+generator = fibonacci()
+for i in range(1):
+    print(next(generator))
